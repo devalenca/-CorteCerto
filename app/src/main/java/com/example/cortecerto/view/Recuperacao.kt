8af19f526edee097531b39bf3818c97d112a4db7
Grilo <elizeu.antunes03@gmail.com>
2023-11-08 21:30:33 -0300

    recuperação de senha

--- app/src/main/java/com/example/cortecerto/view/Recuperacao.kt
+++ app/src/main/java/com/example/cortecerto/view/Recuperacao.kt
@@ -1,4 +1,44 @@
 package com.example.cortecerto.view
 
-class Recuperacao {
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cortecerto.databinding.ActivityRecuperacaoBinding
+import com.google.android.gms.tasks.OnSuccessListener
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+class Recuperacao : AppCompatActivity() {
+    private lateinit var binding: ActivityRecuperacaoBinding
+    private val auth = FirebaseAuth.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRecuperacaoBinding.inflate((layoutInflater))
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        binding.btnEnviar.setOnClickListener {
+            val email = binding.editEmail.text.toString()
+            auth.sendPasswordResetEmail(email)
+                .addOnSuccessListener {
+                    // Sucesso ao enviar o email de redefinição de senha
+                    Toast.makeText(
+                        this,
+                        "Email de redefinição de senha enviado com sucesso.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                .addOnFailureListener { e ->
+                    // Falha ao enviar o email de redefinição de senha
+                    Toast.makeText(
+                        this,
+                        "Falha ao enviar o email de redefinição de senha: ${e.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+
+        }
+    }
 }
